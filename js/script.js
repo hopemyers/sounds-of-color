c9dd241f53cddc842ad65d0e417699705394069c
Hope Myers <hopemyers@Hopes-MacBook-Air.local>
2019-04-10 13:10:16 +0800

    initial commit

---
+++ js/script.js
@@ -0,0 +1,215 @@
+function redbutton() {
+  var redaudio = document.getElementById("redmusic");
+  redaudio.play();
+  document.getElementById('red').style.backgroundColor="#ff1a1a";
+  document.getElementById("orange").style.backgroundColor="#ff3333";
+  document.getElementById("yellow").style.backgroundColor="#ff4d4d";
+  document.getElementById("green").style.backgroundColor="#ff6666";
+  document.getElementById("blue").style.backgroundColor="#ff8080";
+  document.getElementById("purple").style.backgroundColor="#ff9999";
+  document.getElementById("black").style.backgroundColor="#ffcccc";
+  document.getElementById("grey").style.backgroundColor="#ffb3b3";
+  document.getElementById("animation").style.backgroundColor="red";
+}
+function redoff() {
+  var redaudio = document.getElementById("redmusic");
+  redaudio.pause();
+  document.getElementById('red').style.backgroundColor="red";
+  document.getElementById("orange").style.backgroundColor="orange";
+  document.getElementById("yellow").style.backgroundColor="yellow";
+  document.getElementById("green").style.backgroundColor="green";
+  document.getElementById("blue").style.backgroundColor="blue";
+  document.getElementById("purple").style.backgroundColor="purple";
+  document.getElementById("black").style.backgroundColor="black";
+  document.getElementById("grey").style.backgroundColor="grey";
+  document.getElementById("animation").style.backgroundColor="white";
+}
+
+function orangebutton() {
+  var orangeaudio = document.getElementById("orangemusic");
+  orangeaudio.play();
+  document.getElementById('red').style.backgroundColor="#ffedcc";
+  document.getElementById("orange").style.backgroundColor="#ffaf1a";
+  document.getElementById("yellow").style.backgroundColor="#ffb833";
+  document.getElementById("green").style.backgroundColor="#ffc14d";
+  document.getElementById("blue").style.backgroundColor="#ffc966";
+  document.getElementById("purple").style.backgroundColor="#ffd280";
+  document.getElementById("black").style.backgroundColor="#ffe4b3";
+  document.getElementById("grey").style.backgroundColor="#ffdb99";
+  document.getElementById("animation").style.backgroundColor="orange";
+}
+function orangeoff() {
+  var orangeaudio = document.getElementById("orangemusic");
+  orangeaudio.pause();
+  document.getElementById('red').style.backgroundColor="red";
+  document.getElementById("orange").style.backgroundColor="orange";
+  document.getElementById("yellow").style.backgroundColor="yellow";
+  document.getElementById("green").style.backgroundColor="green";
+  document.getElementById("blue").style.backgroundColor="blue";
+  document.getElementById("purple").style.backgroundColor="purple";
+  document.getElementById("black").style.backgroundColor="black";
+  document.getElementById("grey").style.backgroundColor="grey";
+  document.getElementById("animation").style.backgroundColor="white";
+}
+
+function yellowbutton() {
+  var yellowaudio = document.getElementById("yellowmusic");
+  yellowaudio.play();
+  document.getElementById('red').style.backgroundColor="#ffffb3";
+  document.getElementById("orange").style.backgroundColor="#ffffcc";
+  document.getElementById("yellow").style.backgroundColor="#ffff1a";
+  document.getElementById("green").style.backgroundColor="#ffff33";
+  document.getElementById("blue").style.backgroundColor="#ffff4d";
+  document.getElementById("purple").style.backgroundColor="#ffff66";
+  document.getElementById("black").style.backgroundColor="#ffff99";
+  document.getElementById("grey").style.backgroundColor="#ffff80";
+  document.getElementById("animation").style.backgroundColor="yellow";
+}
+function yellowoff() {
+  var yellowaudio = document.getElementById("yellowmusic");
+  yellowaudio.pause();
+  document.getElementById('red').style.backgroundColor="red";
+  document.getElementById("orange").style.backgroundColor="orange";
+  document.getElementById("yellow").style.backgroundColor="yellow";
+  document.getElementById("green").style.backgroundColor="green";
+  document.getElementById("blue").style.backgroundColor="blue";
+  document.getElementById("purple").style.backgroundColor="purple";
+  document.getElementById("black").style.backgroundColor="black";
+  document.getElementById("grey").style.backgroundColor="grey";
+  document.getElementById("animation").style.backgroundColor="white";
+}
+
+function greenbutton() {
+  var greenaudio = document.getElementById("greenmusic");
+  greenaudio.play();
+  document.getElementById('red').style.backgroundColor="#66ff66";
+  document.getElementById("orange").style.backgroundColor="#99ff99";
+  document.getElementById("yellow").style.backgroundColor="#b3ffb3";
+  document.getElementById("green").style.backgroundColor="#009900";
+  document.getElementById("blue").style.backgroundColor="#00b300";
+  document.getElementById("purple").style.backgroundColor="#00cc00";
+  document.getElementById("black").style.backgroundColor="#33ff33";
+  document.getElementById("grey").style.backgroundColor="#00ff00";
+  document.getElementById("animation").style.backgroundColor="green";
+}
+function greenoff() {
+  var greenaudio = document.getElementById("greenmusic");
+  greenaudio.pause();
+  document.getElementById('red').style.backgroundColor="red";
+  document.getElementById("orange").style.backgroundColor="orange";
+  document.getElementById("yellow").style.backgroundColor="yellow";
+  document.getElementById("green").style.backgroundColor="green";
+  document.getElementById("blue").style.backgroundColor="blue";
+  document.getElementById("purple").style.backgroundColor="purple";
+  document.getElementById("black").style.backgroundColor="black";
+  document.getElementById("grey").style.backgroundColor="grey";
+  document.getElementById("animation").style.backgroundColor="white";
+}
+
+function bluebutton() {
+  var blueaudio = document.getElementById("bluemusic");
+  blueaudio.play();
+  document.getElementById('red').style.backgroundColor="#8080ff";
+  document.getElementById("orange").style.backgroundColor="#9999ff";
+  document.getElementById("yellow").style.backgroundColor="#b3b3ff";
+  document.getElementById("green").style.backgroundColor="#ccccff";
+  document.getElementById("blue").style.backgroundColor="#1a1aff";
+  document.getElementById("purple").style.backgroundColor="#3333ff";
+  document.getElementById("black").style.backgroundColor="#6666ff";
+  document.getElementById("grey").style.backgroundColor="#4d4dff";
+  document.getElementById("animation").style.backgroundColor="blue";
+}
+function blueoff() {
+  var blueaudio = document.getElementById("bluemusic");
+  blueaudio.pause();
+  document.getElementById('red').style.backgroundColor="red";
+  document.getElementById("orange").style.backgroundColor="orange";
+  document.getElementById("yellow").style.backgroundColor="yellow";
+  document.getElementById("green").style.backgroundColor="green";
+  document.getElementById("blue").style.backgroundColor="blue";
+  document.getElementById("purple").style.backgroundColor="purple";
+  document.getElementById("black").style.backgroundColor="black";
+  document.getElementById("grey").style.backgroundColor="grey";
+  document.getElementById("animation").style.backgroundColor="white";
+}
+
+function purplebutton() {
+  var purpleaudio = document.getElementById("purplemusic");
+  purpleaudio.play();
+  document.getElementById('red').style.backgroundColor="#ff00ff";
+  document.getElementById("orange").style.backgroundColor="#ff33ff";
+  document.getElementById("yellow").style.backgroundColor="#ff66ff";
+  document.getElementById("green").style.backgroundColor="#ff80ff";
+  document.getElementById("blue").style.backgroundColor="#ff99ff";
+  document.getElementById("purple").style.backgroundColor="#990099";
+  document.getElementById("black").style.backgroundColor="#e600e6";
+  document.getElementById("grey").style.backgroundColor="#b300b3";
+  document.getElementById("animation").style.backgroundColor="purple";
+}
+function purpleoff() {
+  var purpleaudio = document.getElementById("purplemusic");
+  purpleaudio.pause();
+  document.getElementById('red').style.backgroundColor="red";
+  document.getElementById("orange").style.backgroundColor="orange";
+  document.getElementById("yellow").style.backgroundColor="yellow";
+  document.getElementById("green").style.backgroundColor="green";
+  document.getElementById("blue").style.backgroundColor="blue";
+  document.getElementById("purple").style.backgroundColor="purple";
+  document.getElementById("black").style.backgroundColor="black";
+  document.getElementById("grey").style.backgroundColor="grey";
+  document.getElementById("animation").style.backgroundColor="white";
+}
+
+function blackbutton() {
+  var blackaudio = document.getElementById("blackmusic");
+  blackaudio.play();
+  document.getElementById('red').style.backgroundColor="#1a1a1a";
+  document.getElementById("orange").style.backgroundColor="#262626";
+  document.getElementById("yellow").style.backgroundColor="#333333";
+  document.getElementById("green").style.backgroundColor="#404040";
+  document.getElementById("blue").style.backgroundColor="#4d4d4d";
+  document.getElementById("purple").style.backgroundColor="#595959";
+  document.getElementById("black").style.backgroundColor="#0d0d0d";
+  document.getElementById("grey").style.backgroundColor="#666666";
+  document.getElementById("animation").style.backgroundColor="black";
+}
+function blackoff() {
+  var blackaudio = document.getElementById("blackmusic");
+  blackaudio.pause();
+  document.getElementById('red').style.backgroundColor="red";
+  document.getElementById("orange").style.backgroundColor="orange";
+  document.getElementById("yellow").style.backgroundColor="yellow";
+  document.getElementById("green").style.backgroundColor="green";
+  document.getElementById("blue").style.backgroundColor="blue";
+  document.getElementById("purple").style.backgroundColor="purple";
+  document.getElementById("black").style.backgroundColor="black";
+  document.getElementById("grey").style.backgroundColor="grey";
+  document.getElementById("animation").style.backgroundColor="white";
+}
+
+function greybutton() {
+  var greyaudio = document.getElementById("greymusic");
+  greyaudio.play();
+  document.getElementById('red').style.backgroundColor="#a6a6a6";
+  document.getElementById("orange").style.backgroundColor="#b3b3b3";
+  document.getElementById("yellow").style.backgroundColor="#bfbfbf";
+  document.getElementById("green").style.backgroundColor="#cccccc";
+  document.getElementById("blue").style.backgroundColor="#d9d9d9";
+  document.getElementById("purple").style.backgroundColor="#e6e6e6";
+  document.getElementById("black").style.backgroundColor="#999999";
+  document.getElementById("grey").style.backgroundColor="#8c8c8c";
+  document.getElementById("animation").style.backgroundColor="grey";
+}
+function greyoff() {
+  var greyaudio = document.getElementById("greymusic");
+  greyaudio.pause();
+  document.getElementById('red').style.backgroundColor="red";
+  document.getElementById("orange").style.backgroundColor="orange";
+  document.getElementById("yellow").style.backgroundColor="yellow";
+  document.getElementById("green").style.backgroundColor="green";
+  document.getElementById("blue").style.backgroundColor="blue";
+  document.getElementById("purple").style.backgroundColor="purple";
+  document.getElementById("black").style.backgroundColor="black";
+  document.getElementById("grey").style.backgroundColor="grey";
+  document.getElementById("animation").style.backgroundColor="white";
+}
